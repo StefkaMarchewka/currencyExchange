0f4d7aaa60046b18dce299d91168beaebc7bb2d8
Stefania Hrycyk Kubat <stefania.hrycyk@gmail.com>
2020-01-07 14:53:30 +0100

    inicial commit

---
+++ src/main/java/com/codecool/curencyexchange/CurencyexchangeApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.curencyexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurencyexchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CurencyexchangeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codecool/curencyexchange/CurencyexchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.curencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CurencyexchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
